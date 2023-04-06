d39cadb94663ee139d2dd82fc27f299cdcad6d56
Frankie Silva Santos <109253810+FrankieSilvaSantos@users.noreply.github.com>
2023-04-06 15:47:51 -0300

    Add files via upload

---
+++ javascriptCoder2/strings.js
@@ -0,0 +1,15 @@
+const escola = "Coder"
+
+console.log(escola.charAt(4))
+
+console.log(escola.indexOf('e'))
+
+console.log(escola.substring(1))
+
+console.log(escola.substring(0,3))
+
+console.log("escola".concat(escola).concat("!"))
+
+console.log(escola.replace(3, "3"))
+
+console.log("ana,maria,joana".split(','))
